c535e1dc3145e1eef2b82a81115ee25a7e7177bd
x95castle1 <x95castle1@yahoo.com>
2023-06-06 17:03:13 -0500

    Added Joiner Stuff

--- src/main/java/com/vmware/tanzu/apps/sso/accelerator/web/AuthenticatedHomeController.java
+++ src/main/java/com/vmware/tanzu/apps/sso/accelerator/web/AuthenticatedHomeController.java
@@ -5,6 +5,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.google.common.base.Joiner;
 
 @Controller
 public class AuthenticatedHomeController {
@@ -15,6 +16,8 @@ public class AuthenticatedHomeController {
 
 		authenticatedUser.getClaims().keySet().stream()
                 .forEach(System.out::println);
+
+								model.addAttribute("all_claims", Joiner.on(", ").withKeyValueSeparator(": ").join(authenticatedUser.getClaims()));
 
 		return "authenticated-home.html";
 	}
